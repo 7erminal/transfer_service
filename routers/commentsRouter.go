dca192c28671347099946c3c8772bbf69fa091e5
Bede Abbe <bede.abbe@gmail.com>
2025-12-16 13:10:17 +0000

    changes

---
+++ routers/commentsRouter.go
@@ -0,0 +1,181 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["transfer_service/controllers:CallbackController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:CallbackController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:ObjectController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:objectId`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:TransferController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:TransferController"],
+        beego.ControllerComments{
+            Method: "TransferCommission",
+            Router: `/transfer-commission/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Get",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:uid`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Login",
+            Router: `/login`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["transfer_service/controllers:UserController"] = append(beego.GlobalControllerRouter["transfer_service/controllers:UserController"],
+        beego.ControllerComments{
+            Method: "Logout",
+            Router: `/logout`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
